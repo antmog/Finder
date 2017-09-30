4d0a38be4aa7bb2ff2e0bd87f860b979f5eae733
unknown <antmogbox@yahoo.com>
2017-09-30 18:21:07 +0300

    reformatted code added javadocs fixed warnings deleted debug code

--- src/main/java/finder/util/ResultTreeCreateLogic.java
+++ src/main/java/finder/util/ResultTreeCreateLogic.java
@@ -6,6 +6,7 @@ import javafx.scene.control.TreeItem;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 
 public class ResultTreeCreateLogic {
     /**
@@ -13,25 +14,26 @@ public class ResultTreeCreateLogic {
      *
      * @param listOfDirs List of directories containing files we search for (and files).
      * @param sb         Text to search.
-     * @param extensions Extensions of target files.
      */
-    public static void showResultFileTree(StringBuffer sb, ArrayList<File> listOfDirs, Object[] extensions, FinderInstance finderInstance) {
+    public static void showResultFileTree(StringBuffer sb, List<File> listOfDirs, FinderInstance finderInstance) {
         // creating new tree from root of main tree (where folders to search are selected)
         // suppress warning? i swear finderInstance.getFileTree().getRoot() is a tree item
-        TreeItem<String> resultRootItem = new TreeItem<String>(((CheckBoxTreeItem<String>) finderInstance.getFileTree().getRoot()).getValue());
+        TreeItem<String> resultRootItem = new TreeItem<>((finderInstance.getFileTree().getRoot()).getValue());
         generateResultTree(resultRootItem, listOfDirs);
         finderInstance.getResultFileTree().setSearchText(sb.toString());
+        // clear result tree
         finderInstance.getResultFileTree().setRoot(resultRootItem);
     }
 
     /**
-     * Adding search results one by one INCLUDING FILEPATH as tree nodes (if we have D:\folder1\file1 and D:\folder1\file2 then manually adding
+     * Adding search results one by one INCLUDING FILEPATH as tree nodes (if we have D:\folder1\file1 and
+     * D:\folder1\file2 then manually adding
      * full file path as tree nodes: D:\ D:\folder1 D:\folder1\file1/2( + checking if tree node alrdy exists) ).
      *
      * @param resultRootItem Result root item for result tree.
      * @param listOfDirs     Result of search.
      */
-    private static void generateResultTree(TreeItem<String> resultRootItem, ArrayList<File> listOfDirs) {
+    private static void generateResultTree(TreeItem<String> resultRootItem, List<File> listOfDirs) {
         ArrayList<CheckBoxTreeItem<String>> listOfItems = new ArrayList<>(); // temp list to keep full path to file
         // for example for file  "D:\folder1\file1" : "D:\" + "D:\folder1" + "D:\folder1\file1"
 
@@ -43,17 +45,18 @@ public class ResultTreeCreateLogic {
                 listOfItems.add(new CheckBoxTreeItem<>(file.getAbsolutePath()));
             }
             // adding current item
-            addItem(listOfItems, resultRootItem);
+            addItems(listOfItems, resultRootItem);
         }
     }
 
     /**
-     * Adding item to result tree.
+     * Adding items to result tree.
      *
-     * @param listOfItems    Structure of item: for example for file "D:\folder1\file1" its : "D:\" + "D:\folder1" + "D:\folder1\file1".
+     * @param listOfItems    Structure of item: for example for file "D:\folder1\file1" its :
+     *                      "D:\" + "D:\folder1" + "D:\folder1\file1".
      * @param resultRootItem Root item for result tree.
      */
-    private static void addItem(ArrayList<CheckBoxTreeItem<String>> listOfItems, TreeItem<String> resultRootItem) {
+    private static void addItems(List<CheckBoxTreeItem<String>> listOfItems, TreeItem<String> resultRootItem) {
         boolean noRepeat = true; // variable to check if tree node alrdy exists
         if (!listOfItems.isEmpty()) {
             // another item to check/add (compare with current items and check if it already exists)
@@ -61,22 +64,22 @@ public class ResultTreeCreateLogic {
             File compareItemFile = new File(compareItem.getValue());
             if (resultRootItem.getChildren().isEmpty()) {
                 // if tree node has no children - the tree node we are going to add 100% doesn't exist
-                addSmartItem(resultRootItem, compareItemFile);
+                addItem(resultRootItem, compareItemFile);
             } else {
                 // otherwise we check it manually
                 for (TreeItem<String> item : resultRootItem.getChildren()) {
-                    if (item.getValue().equals(OtherLogic.getShortFileName(compareItemFile))) {
+                    if (item.getValue().equals(FileSystemLogic.getShortFileName(compareItemFile))) {
                         noRepeat = false;
                         break;
                     }
                 }
                 // and if it doesn't exist
                 if (noRepeat) {
-                    addSmartItem(resultRootItem, compareItemFile);
+                    addItem(resultRootItem, compareItemFile);
                 }
             }
             resultRootItem = resultRootItem.getChildren().get(resultRootItem.getChildren().size() - 1);
-            addItem(listOfItems, resultRootItem);
+            addItems(listOfItems, resultRootItem);
         }
     }
 
@@ -86,17 +89,12 @@ public class ResultTreeCreateLogic {
      * @param resultRootItem Root item for result tree.
      * @param item           Item to be added.
      */
-    private static void addSmartItem(TreeItem<String> resultRootItem, File item) {
-        TreeItem<String> treeItem = new TreeItem<>(OtherLogic.getShortFileName(item));
+    private static void addItem(TreeItem<String> resultRootItem, File item) {
+        TreeItem<String> treeItem = new TreeItem<>(FileSystemLogic.getShortFileName(item));
         resultRootItem.getChildren().add(treeItem);
         if (item.isFile()) {
             // Any custom visualisation for tree node wich is not folder (file) (for better view).
-            /*
-            try {
-                treeItem.setGraphic(new FXMLLoader(Finder.class.getResource(Resources.IMG + "icon.png")).load());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }*/
+            // ->>
             // expands all parent directories of file found
             while (treeItem.getParent() != null) {
                 treeItem = treeItem.getParent();
